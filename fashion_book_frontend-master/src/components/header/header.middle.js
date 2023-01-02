5ae6925fcffd7a0078ca66f56a683455ef404b56
thedong <nguyenthedong8501@gmail.com>
2023-01-02 20:36:30 +0700

    first commit

---
+++ fashion_book_frontend-master/src/components/header/header.middle.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import storeConfig from "../../config/storage.config";
+
+class HeaderMiddle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "Account"
+    };
+  }
+  componentWillMount() {
+    if (storeConfig.getUser() !== null) {
+      this.setState({
+        email: storeConfig.getUser().email
+      });
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.islogin) {
+      this.setState({
+        email: "Account"
+      });
+    } else {
+      this.setState({
+        email: storeConfig.getUser().email
+      });
+    }
+  }
+  handlelogin = () => {
+    if (this.props.islogin) {
+      return (
+        // {/* <li>
+        //             <Link to={"/cart"}>
+        //               <i className="fa fa-shopping-cart" />
+        //             </Link>
+        //           </li> */}
+        <li
+        
+          className="btn-custom"
+          onClick={() => {
+            window.location.reload();
+            this.props.logout();
+            this.props.history.push("/");
+          }}
+        >
+          {/* <Link to={"/cart"}>
+                      <i className="fa fa-shopping-cart" />
+          </Link> */}
+          <a>
+            <i className="fa fa-sign-out"/>Đăng xuất
+          </a>
+        </li>
+      );
+    } else {
+      return (
+        <li>
+          <Link to="/login_register">
+            <i className="fa fa-sign-in " />Đăng nhập
+          </Link>
+        </li>
+      );
+    }
+  };
+  handleProfile = () => {
+    if (this.state.email === "Account") {
+      return;
+    } else {
+        
+      this.props.history.push("/profile/" + this.state.email);
+    }
+  };
+  hoverlogin = () =>{
+    if(this.props.islogin){
+      return (
+        <ul className='sub-menu'>
+
+<li   onClick={() => this.handleProfile()}>
+                <Link to={"/"}  >Hồ Sơ </Link>
+              </li>0
+
+              <li><Link to='/purchase_history' >Đơn Hàng </Link></li>
+          
+          </ul>
+      );
+    }
+  }
+  render() {
+    return (
+      <div className="header-middle">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="logo pull-left">
+                <a href="/">
+                  <img src="/assets/images/home/logo1.gif" alt="" />
+                </a>
+              </div>
+             
+            </div>
+            <div className="col-sm-8">
+              <div className="shop-menu pull-right">
+                <ul className="nav navbar-nav collapse navbar-collapse">
+                <li className='dropdown'>
+                    <a className='Setting-item'>
+                      <i className="fa fa-user dropbtn"></i>
+                    </a>
+                      {this.hoverlogin()}
+                  </li>
+                 
+                
+                  <li>
+                    <Link to={"/cart"}>
+                      <i className="fa fa-shopping-cart" />
+                    </Link>
+                  </li>
+                  {this.handlelogin()}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HeaderMiddle;
